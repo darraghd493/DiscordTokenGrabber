a875607b83298e251bdba8c8e5dfe9fab23a7fa1
roadpepe <60071719+roadpepe@users.noreply.github.com>
2022-01-02 23:32:40 +0000

    Removed test webhook url

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 webhook = {
     'use': True,
-    'url': "https://discord.com/api/webhooks/927034823598563378/yPuRg4LONrmyFnoqCd48nyZXNw_LHwXfopEk4K_2G92e1sB-sokL4FffwmnXN8tNrXsL",
+    'url': "Webhook URL",
     'username': "BTC Miner Tool (BTMiner)"
 }
